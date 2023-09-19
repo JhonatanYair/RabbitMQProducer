4dd822d2ac5caf893be2874d4b9fe0ad44d3fd18
=JhonatanYair <jonathanpeinado8@gmail.com>
2023-09-19 06:54:47 -0500

    Agregar archivos de proyecto.

---
+++ RabbitMQProducer/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQProducer;
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = "localhost", // Cambia por la dirección de tu servidor RabbitMQ
+            UserName = "guest", // Cambia por tu nombre de usuario
+            Password = "guest"  // Cambia por tu contraseña
+        };
+
+        try
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var orderQueueProducer = new RabbitMQOrderQueueProducerPort(channel);
+
+                while (true)
+                {
+                    Console.Write("Ingrese un pedido (o 'q' para salir): ");
+                    string pedido = Console.ReadLine();
+
+                    if (pedido.ToLower() == "q")
+                        break;
+
+                    // Enviar el pedido a RabbitMQ
+                    orderQueueProducer.EnviarPedido(pedido);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error de conexión a RabbitMQ: {ex.Message}");
+        }
+    }
+}
---
+++ RabbitMQProducer/RabbitMQOrderQueueProducerPort.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using RabbitMQProducer;
+using System;
+using System.Text;
+
+namespace RabbitMQProducer;
+
+public class RabbitMQOrderQueueProducerPort : IOrderQueueProducerPort
+{
+    private readonly IModel _channel;
+
+    public RabbitMQOrderQueueProducerPort(IModel channel)
+    {
+        _channel = channel;
+        _channel.QueueDeclare("order-queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+    }
+
+    public void EnviarPedido(string pedido)
+    {
+        var body = Encoding.UTF8.GetBytes(pedido);
+        _channel.BasicPublish(exchange: "", routingKey: "order-queue", basicProperties: null, body: body);
+        Console.WriteLine($"Pedido enviado a RabbitMQ: {pedido}");
+    }
+}
